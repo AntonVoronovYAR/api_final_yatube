f5afa98981ee5be1153934b0ea7c90119a7fb3c2
Воронов Антон <Antonuo89@gmail.com>
2023-03-16 11:35:05 +0300

    Отправка на первое код-ревью

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,12 +1,13 @@
+import os
+
 from pathlib import Path
 from datetime import timedelta
-
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Защитил ключ с помощью dotenv, но были ошибки
-# на платформе яндекс-практикума, не мог сдать на код-ревью
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 DEBUG = True
 
