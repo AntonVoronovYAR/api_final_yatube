e6ac4b5717de18dfacbdd81cd6b6db31b243e0e9
Воронов Антон <Antonuo89@gmail.com>
2023-03-16 11:16:09 +0300

    Отправка на первое код-ревью

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = str(os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT'))
 
 DEBUG = True
 
